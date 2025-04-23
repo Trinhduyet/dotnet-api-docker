bdb7ae9f4fd4b6a777357eb3f5d9f023ca966524
yopaz-trinhnv <trinhnv@yopaz.vn>
2025-04-23 16:53:19 +0700

    test

--- MyApi/Program.cs
+++ MyApi/Program.cs
@@ -33,10 +33,10 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
-app.MapGet("/ping", () => Results.Ok("pong"))
+app.MapGet("/ping", () => Results.Ok("test"))
    .WithName("Ping")
    .WithOpenApi();
-   
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
